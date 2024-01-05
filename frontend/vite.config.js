681df427e5ba60cc3e09b4a3c2b3588d6bd9642c
Kent McKinney <kent.p.mckinney@gmail.com>
2024-01-05 06:26:27 -0800

    fix vite configuration so that the backend can serve static files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,46 @@
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import { join } from "node:path";
 import { buildSync } from "esbuild";
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   root: __dirname,
   build: {
-    outDir: '../../dist/apps/my-app',
+    outDir: "dist/apps/rcr",
     reportCompressedSize: true,
     commonjsOptions: {
-      transformMixedEsModules: true,
-    },
+      transformMixedEsModules: true
+    }
   },
-  cacheDir: '../../node_modules/.vite/my-app',
+  cacheDir: "node_modules/.vite/rcr",
   server: {
     port: 3000,
-    host: 'localhost',
+    host: "localhost",
     proxy: {
-      '/api': {
-        target: 'http://localhost:5900/',
+      "/api": {
+        target: "http://localhost:5900/",
         changeOrigin: true,
         secure: false,
         ws: true,
-        rewrite: (path) => path.replace(/^\/app/, '')
+        rewrite: (path) => path.replace(/^\/app/, "")
       }
     },
-    '/socket.io': {
-      target: 'ws://localhost:3000',
-      ws: true,
+    "/socket.io": {
+      target: "ws://localhost:3000",
+      ws: true
     }
   },
 
   preview: {
     port: 3001,
-    host: 'localhost',
+    host: "localhost"
   },
 
   plugins: [
     react(),
     svgr({
-      include: '**/*.svg?react',
+      include: "**/*.svg?react"
     }),
     {
       apply: "build",
@@ -50,9 +50,9 @@ export default defineConfig({
           minify: true,
           bundle: true,
           entryPoints: [join(process.cwd(), "serviceWorker.js")],
-          outfile: join(process.cwd(), "dist", "serviceWorker.js"),
+          outfile: join(process.cwd(), "dist", "serviceWorker.js")
         });
-      },
-    },
+      }
+    }
   ]
 });
